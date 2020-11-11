7e58458a600a15f3c061fba6199180697659063e
somin <qkrthals0524@naver.com>
2020-11-11 12:45:54 +0900

    #2 Movies Service

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,37 +1,36 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 import { publicDecrypt } from 'crypto';
+import { Movie } from './entities/movie.entitiy';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+
+        constructor(private readonly moviesService: MoviesService){}
+
     @Get()
-    getAll(){
-        return "This will return all movies";
+    getAll():Movie[]{
+        return this.moviesService.getAll();
     }
-    @Get("search")
-    search(@Query("year") seachingYear: string){
-        return `We are searching for a movie made after: ${seachingYear} `;
-    }
-    @Get("/:id")
-    getOne(@Param("id") movieId:string){
-        return `This will return one movie with the id: ${movieId}`
+   
+    @Get(":id")
+    getOne(@Param("id") movieId:string):Movie{
+        return this.moviesService.getOne(movieId)
     }
 
     @Post()
     create(@Body() movieData){
-        return movieData;
+        return this.moviesService.create(movieData);
     }
 
     @Delete("/:id")
     remove(@Param('id') movieId:string){
-        return `This will delete a movie with the id: ${movieId}`;
+        return this.moviesService.deleteOne(movieId);
     }
 
     @Patch('/:id')
     path(@Param('id') movieId:string, @Body() updateData){
-        return {
-            updateMovie:movieId,
-            ...updateData,
-        }
+       return this.moviesService.updata(movieId,updateData)
     }
 
 
